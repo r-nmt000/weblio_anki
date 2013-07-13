a169311e19b7b54f9bf94719701baf4bb3894bbb
Ryusuke Nomoto <r.nmt000@gmail.com>
2013-07-13 13:13:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+weblio_anki
+===========
+
+this is a bookmarklet which parses weblio-wordbook page and output
+
+"English Sentence","Pronunciation", "Japanese meaning of a word", "Japanese Sentence"
+
+in csv format on top of the page.
--- weblio_anki.js
+++ weblio_anki.js
@@ -1,46 +1,31 @@
-javascript:
-alert( function (){
-    var Sentences = document.getElementsByClassName("tngMainTSRH");
-    var Meanings  = document.getElementsByClassName("tngMainTSRFL");
-    var Numbers = (Sentences.length)*2;
-    var OutputData(Numbers);
-    for(i=0; i < Numbers; i++){
-	//outputdataに要素をぶっこんでく	
-	OutputData[i] = ((Sentences[i].firstChild).textContent);
-	OutputData[i+1] = ((Meanings[i].firstChild).textContent);
-	
-
-	//きれいに表示されるよう整理
-    }
-    return OutputData;
-}    
-)
-
-
-
 javascript:(function(){
-    var text='';
-    $('table.tngMainT tr').each(function(){
-	var line='';
-	$(this).find('th,td').filter(function(){
-	    var cn=$(this).attr('class');
-	    return!cn.match(/tngMain[TH](VC|SH|SS|TS|CL|MK|CK|DT|LV|TG)/i);
-	}).each(function(){
-	    var cn=$(this).attr('class');
-	    if(cn == "tngMainTSR"){
-		
-		&(this).find('.tngMainTSRG,.tngMainTSRFL').each(function(){
-		    line+='"'+$(this).text().replace(/\"/g,'\"\"')+'",';
-		}
-	    }else{
-       		line+='"'+$(this).text().replace(/\"/g,'\"\"')+'",';
-	    }
-	});
-	text+=line.replace(/,$/,'')+'\r\n';
-    });
-    if(text){
-	$('#csv').remove();
-	$('body').prepend('<textarea id="csv"style="width:100%;height:10em;"></textarea>');
-	$('#csv').val(text);
-    }
+  var text='';
+  $('table.tngMainT tr').each(function(){
+    var line='';
+    var pronanciation='';
+    var translation='';
+    var Esentence='';
+    var Jsentence='';
+    $(this).find('th,td').filter(function(){
+      var cn=$(this).attr('class');
+      return!cn.match(/tngMain[TH](VC|SH|SS|TS|CL|MK|CK|DT|LV|TG)/i);
+    }).each(function(){
+      var cn=$(this).attr('class');
+      if(cn == "tngMainTSR"){$(this).find('.tngMainTSRH').each(function(){
+        line+='"'+$(this).text().replace(/\"/g,'\"\"')+'",';
+	line+=pronanciation+translation;});
+	$(this).find('.tngMainTSRFL').each(function(){
+	  line+='"'+$(this).text().replace(/\"/g,'\"\"')+'",';});
+	}else if(cn == 'tngMainTHT'){
+          pronanciation='"'+$(this).text().replace(/\"/g,'\"\"')+'",';
+	}else if(cn == 'tngMainTIM'){
+          translation='"'+$(this).text().replace(/\"/g,'\"\"')+'",';
+	}
+     });
+     text+=line.replace(/,$/,'')+'\r\n';
+  });
+  if(text){
+    $('#csv').remove();$('body').prepend('<textarea id="csv"style="width:100%;height:10em;"></textarea>');
+    $('#csv').val(text);
+  }
 })()
