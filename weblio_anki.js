40aa792811a63aa2e320a2d09e85334f5fe536f7
Ryusuke Nomoto <r.nmt000@gmail.com>
2014-11-24 14:50:43 +0900

    fix a bug that words don't appear when japanese words are registered

--- weblio_anki.js
+++ weblio_anki.js
@@ -2,25 +2,24 @@ javascript:(function(){
   var text = '';
   $('table.tngMainT tr').each(function(){
     var line = '';
-    var pronanciation = '';
-    var translation = '';
-    var Esentence = '';
-    var Jsentence = '';
+    var pronunciation = '';
+    var meaning = '';
     $(this).find('th,td').filter(function(){
       var cn = $(this).attr('class');
       return!cn.match(/tngMain[TH](VC|SH|SS|TS|CL|MK|CK|DT|LV|TG)/i);
     }).each(function(){
       var cn = $(this).attr('class');
-      if(cn == "tngMainTSR"){$(this).find('.tngMainTSRH').each(function(){
-        line += '"'+$(this).text().replace(/\"/g,'\"\"')+'",';
-	line += pronanciation+translation;});
-	$(this).find('.tngMainTSRFL').each(function(){
-	  line += '"'+$(this).text().replace(/\"/g,'\"\"')+'",';});
-	}else if(cn == 'tngMainTHT'){
-          pronanciation = '"'+$(this).text().replace(/\"/g,'\"\"')+'",';
-	}else if(cn == 'tngMainTIM'){
-          translation = '"'+$(this).text().replace(/\"/g,'\"\"')+'",';
-	}
+      if(cn == "tngMainTSR"){
+        $(this).find('.tngMainTSRH, .tngMainTSRHJ').each(function(){
+          line += '"'+$(this).text().replace(/\"/g,'\"\"')+'";';
+          line += pronunciation+meaning;});
+        $(this).find('.tngMainTSRFL, .tngMainTSRFJ').each(function(){
+          line += '"'+$(this).text().replace(/\"/g,'\"\"')+'";';});
+      }else if(cn == 'tngMainTHT'){
+        pronunciation= '"'+$(this).text().replace(/\"/g,'\"\"')+'";';
+      }else if(cn == 'tngMainTIM'){
+        meaning = '"'+$(this).text().replace(/\"/g,'\"\"')+'";';
+      }
      });
      text += line.replace(/,$/,'')+'\r\n';
   });
